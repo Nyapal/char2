bfa1b53a71ec83278b32298f38e538d62f31a475
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-11-20 15:40:45 -0800

    client can see all charities

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.get('/', (req, res) => {
   res.render('home', { msg: 'Nya has the fattest pussy' });
 })
 
+// app.js
+
+// OUR MOCK ARRAY OF PROJECTS
+let charities = [
+  { org: "SF Childrens Hospital"},
+  { org: "Orange Juice for All"}
+]
+
+// INDEX
+app.get('/charities', (req, res) => {
+  res.render('charities-index', { charities: charities });
+})
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
