27ac351180183f4c20a046120cd222b6e8a958d9
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-11-20 16:18:51 -0800

    added mongodb & charity model

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/charity-term2');
+
+const Charity = mongoose.model('Charity', {
+    org: String
+});
+
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
@@ -10,7 +17,13 @@ app.set('view engine', 'hbs');
 // })
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Nya has the fattest pussy' });
+  Charity.find()
+    .then(charities => {
+        res.render('charities-index', {charities: charities})
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 // app.js
